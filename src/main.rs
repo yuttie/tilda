c96c2fee5a0ee1a9ce52784b7c32c6a6a129748d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-01-02 20:34:33 +0900

    Randomly initialize variables
    This commit also simplify the initialization code.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::io::{self, BufReader, BufRead};
 use std::iter;
 
 use rand::{Closed01, Rng};
-use rand::distributions::{IndependentSample, Sample, Gamma, LogNormal, RandSample};
+use rand::distributions::{IndependentSample, Sample, Gamma, LogNormal, RandSample, Range};
 
 
 type Bag = HashMap<usize, usize>;
@@ -128,95 +128,71 @@ impl IndependentSample<Vec<f64>> for Dirichlet {
 }
 
 fn lda(dataset: Vec<Bag>, alpha: Vec<f64>, beta: Vec<f64>, burn_in: usize, num_samples: usize) {
-    // Initialization
+    let num_docs: usize = dataset.len();
     let num_topics: usize = alpha.len();
     let vocab_size: usize = beta.len();
+    let mut rng = rand::thread_rng();
     println!("K = {}", num_topics);
-    println!("M = {}", &dataset.len());
+    println!("M = {}", num_docs);
     println!("V = {}", vocab_size);
 
-    let mut w: Vec<Vec<usize>> = Vec::with_capacity(dataset.len());
-    let mut z: Vec<Vec<usize>> = Vec::with_capacity(dataset.len());
-    let mut z_samples: Vec<Vec<Vec<usize>>> = Vec::with_capacity(dataset.len());
-    let mut theta: Vec<Vec<f64>> = Vec::with_capacity(dataset.len());
-    let mut ndk: Vec<Vec<usize>> = Vec::with_capacity(dataset.len());
+    // Construct zero-filled nested arrays
+    let mut w:     Vec<Vec<usize>> = Vec::with_capacity(num_docs);
+    let mut z:     Vec<Vec<usize>> = Vec::with_capacity(num_docs);
+    let mut theta: Vec<Vec<f64>>   = Vec::with_capacity(num_docs);
+    let mut phi:   Vec<Vec<f64>>   = Vec::with_capacity(num_topics);
+    let mut ndk:   Vec<Vec<usize>> = Vec::with_capacity(num_docs);
+    let mut nkv:   Vec<Vec<usize>> = Vec::with_capacity(num_topics);
+    let mut z_samples: Vec<Vec<Vec<usize>>> = Vec::with_capacity(num_docs);
     for bag in &dataset {
         // n_d
         let mut n_d = 0;
         for &count in bag.values() {
             n_d += count;
         }
-        // w
-        let mut w_d = Vec::with_capacity(n_d);
-        for (&index, &count) in bag {
-            for _ in 0..count {
-                w_d.push(index);
-            }
-        }
-        w.push(w_d);
-        // z
-        let mut z_d = Vec::with_capacity(n_d);
-        let mut z_samples_d = Vec::with_capacity(n_d);
-        for &count in bag.values() {
-            for _ in 0..count {
-                z_d.push(0);
-                let zeros: Vec<usize> = iter::repeat(0).take(num_topics).collect();
-                z_samples_d.push(zeros);
-            }
-        }
-        z.push(z_d);
-        z_samples.push(z_samples_d);
-        // theta
-        let mut theta_d = Vec::with_capacity(num_topics);
-        for _k in 0..num_topics {
-            theta_d.push(1.0 / num_topics as f64);
-        }
-        theta.push(theta_d);
-        // ndk
-        let mut ndk_d = Vec::with_capacity(num_topics);
-        ndk_d.push(n_d);
-        for _k in 1..num_topics {
-            ndk_d.push(0);
-        }
-        ndk.push(ndk_d);
+        w.push(vec![0; n_d]);
+        z.push(vec![0; n_d]);
+        theta.push(vec![0.0; num_topics]);
+        ndk.push(vec![0; num_topics]);
+        z_samples.push(vec![vec![0; num_topics]; n_d]);
     }
+    for k in 0..num_topics {
+        phi.push(vec![0.0; vocab_size]);
+        nkv.push(vec![0; vocab_size]);
+    }
+
+    // Initialize w, z, ndk and nkv
+    let among_topics = Range::new(0, num_topics);
+    for (d, bag) in dataset.iter().enumerate() {
+        let mut i = 0;
+        for (&v, &count) in bag {
+            for _ in 0..count {
+                w[d][i] = v;
+                let k = among_topics.ind_sample(&mut rng);
+                z[d][i] = k;
+                ndk[d][k] += 1;
+                nkv[k][v] += 1;
+                i += 1;
+            }
+        }
+    }
+    // Initialize theta, phi
+    for d in 0..num_docs {
+        let dir = Dirichlet::new(alpha.clone());
+        theta[d] = dir.ind_sample(&mut rng);
+    }
+    for k in 0..num_topics {
+        let dir = Dirichlet::new(beta.clone());
+        phi[k] = dir.ind_sample(&mut rng);
+    }
+
     println!("z = {:?}", z);
     println!("theta = {:?}", theta);
     println!("ndk = {:?}", ndk);
-
-    let mut phi: Vec<Vec<f64>> = Vec::with_capacity(num_topics);
-    let mut nkv: Vec<Vec<usize>> = Vec::with_capacity(num_topics);
-    // phi
-    for _k in 0..num_topics {
-        let mut phi_k = Vec::with_capacity(vocab_size);
-        for _v in 0..vocab_size {
-            phi_k.push(1.0 / vocab_size as f64);
-        }
-        phi.push(phi_k);
-    }
-    // nkv
-    let mut nkv_0 = Vec::with_capacity(vocab_size);
-    for _v in 0..vocab_size {
-        nkv_0.push(0);
-    }
-    for bag in &dataset {
-        for (&index, &count) in bag {
-            nkv_0[index] += count;
-        }
-    }
-    nkv.push(nkv_0);
-    for _k in 1..num_topics {
-        let mut nkv_k = Vec::with_capacity(vocab_size);
-        for _v in 0..vocab_size {
-            nkv_k.push(0);
-        }
-        nkv.push(nkv_k);
-    }
     println!("phi = {:?}", phi);
     println!("nkv = {:?}", nkv);
 
     // Sampling
-    let mut rng = rand::thread_rng();
     for s in 0..(burn_in + num_samples) {
         for (d, w_d) in w.iter().enumerate() {
             for (i, &w_di) in w_d.iter().enumerate() {
